04f2a14e4b765962130cfcc8623a5b0f9efce2cb
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2021-12-20 12:28:09 -0600

    Agregando primeras configuraciones del sitio

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const router = require('./routes');
+require('dotenv').config({ path: 'variables.env'});
+
+const app = express();
+
+//Habilitar EJS como template engine
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Definir ubicación de las vistas
+app.set('views', path.join(__dirname, './views'));
+
+//Archivos estáticos
+app.use(express.static('public'));
+
+//Middleware (usuario logueado, flash messages, fecha actual)
+app.use((req, res, next) =>{
+    const fecha = new Date();
+    res.locals.year = fecha.getFullYear();
+
+    next();
+});
+
+//Routing
+app.use('/', router());
+
+//Agrega el puerto
+app.listen(process.env.PORT, () => {
+    console.log('El servidor está funcionando correctamente');
+});
