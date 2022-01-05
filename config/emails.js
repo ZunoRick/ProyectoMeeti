c2c2165612b9099fb50098f47ecda5fc8e133ab2
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-01-04 23:53:12 -0600

    Modificando configuración de correo

--- config/emails.js
+++ config/emails.js
@@ -1,8 +1,8 @@
 module.exports = {
-	host: 'smtp.mailtrap.io',
-	port: 2525,
+	host: 'smtp.gmail.com',
+	port: 587,
 	auth: {
-		user: '6183892eb1c76b',
-		pass: 'dc76e9e4748836',
+		user: 'ricardozuno14@gmail.com',
+		pass: 'ironrizs140494',
 	},
 };
